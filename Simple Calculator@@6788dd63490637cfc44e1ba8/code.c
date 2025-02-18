c8027d739ac312b7f66ef81a3cbbd0d8cc1dfebc
Gunika-06 <gunika1162.becse24@chitkara.edu.in>
2025-02-18 10:58:09 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int x,y,result;
+    scanf("%d%d",&x,&y);
+    switch(){
+        case '+'
+        result = x+y;
+        break;
+        case '-'
+        result = x-y;
+        break;
+        case '*'
+        result = x*y;
+        break;
+        case '/'
+        result = x/y;
+        break;
+
+    }
+    return 0;
+}
