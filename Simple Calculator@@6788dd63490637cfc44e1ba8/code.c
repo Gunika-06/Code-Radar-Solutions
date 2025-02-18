352aacbd9219cdd3441bd1e6793519f261d3d738
Gunika-06 <gunika1162.becse24@chitkara.edu.in>
2025-02-18 11:13:13 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -15,9 +15,15 @@ int main(){
         printf("%d\n",x*y);
         break;
         case'/':
+        if(y!=0){
         printf("%d\n",x/y);
+        }else {
+            printf("error");
+        }
         break;
-        
+        default :
+        printf("error");
+        break;
     }
     return 0;
 }
